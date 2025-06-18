acf34bf38842563603ee0d21daaf37cae749eeef
Ivan <bestpsyholog@gmail.com>
2025-06-18 19:57:48 +0300

    special for mark

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import About from './components/About/About'
 import Contacts from './components/Contacts/Contacts'
 import Footer from './components/Footer/Footer'
